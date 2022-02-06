a88e39269cb4ab4b6b6e303161c3732c00b3c676
Roberto Martin-Martin <robertom@cs.stanford.edu>
2022-02-06 21:15:38 +0100

    fixes

--- behavior/examples/dataset_generation_observations_example.py
+++ behavior/examples/dataset_generation_observations_example.py
@@ -1,5 +1,7 @@
 import argparse
+import inspect
 import itertools
+import logging
 import os
 
 import h5py
@@ -11,6 +13,7 @@ from igibson.objects.articulated_object import URDFObject
 from igibson.utils import utils
 from igibson.utils.constants import MAX_INSTANCE_COUNT, SemanticClass
 
+import behavior
 from behavior.examples.demo_replay_batch import replay_demo_batch
 
 FRAME_BATCH_SIZE = 100
@@ -28,18 +31,37 @@ def frame_to_entry_idx(frame_count):
     return (frame_count - START_FRAME) // SUBSAMPLE_EVERY_N_FRAMES
 
 
-def parse_args():
-    testdir = os.path.join(igibson.ig_dataset_path, "tests")
-    manifest = os.path.join(testdir, "test_manifest.txt")
-    parser = argparse.ArgumentParser(description="Extract ImVoteNet training data from BEHAVIOR demos in manifest.")
-    parser.add_argument(
-        "--demo_root", type=str, default=testdir, help="Directory containing demos listed in the manifest."
+def parse_args(defaults=False):
+    args_dict = dict()
+    args_dict["demo_root"] = os.path.join(os.path.dirname(inspect.getfile(behavior.examples)), "data")
+    args_dict["log_manifest"] = os.path.join(
+        os.path.dirname(inspect.getfile(behavior.examples)), "data", "test_manifest.txt"
     )
-    parser.add_argument(
-        "--log_manifest", type=str, default=manifest, help="Plain text file consisting of list of demos to replay."
-    )
-    parser.add_argument("--out_dir", type=str, default=testdir, help="Directory to store results in.")
-    return parser.parse_args()
+    args_dict["out_dir"] = os.path.join(os.path.dirname(inspect.getfile(behavior.examples)), "data")
+
+    if not defaults:
+
+        parser = argparse.ArgumentParser(description="Extract ImVoteNet training data from BEHAVIOR demos in manifest.")
+        parser.add_argument(
+            "--demo_root",
+            type=str,
+            default=args_dict["demo_root"],
+            help="Directory containing demos listed in the manifest.",
+        )
+        parser.add_argument(
+            "--log_manifest",
+            type=str,
+            default=args_dict["log_manifest"],
+            help="Plain text file consisting of list of demos to replay.",
+        )
+        parser.add_argument("--out_dir", type=str, default=args_dict["out_dir"], help="Directory to store results in.")
+        args = parser.parse_args()
+        args = parser.parse_args()
+        args_dict["demo_root"] = args.demo_root
+        args_dict["log_manifest"] = args.log_manifest
+        args_dict["out_dir"] = args.out_dir
+
+    return args_dict
 
 
 class PointCloudExtractor(object):
@@ -275,9 +297,14 @@ class BBoxExtractor(object):
 
 
 def main(selection="user", headless=False, short_exec=False):
-    args = parse_args()
+    """
+    Extract a sequence of observations (ImVoteNet format: pointclouds and bounding boxes) from a batch of BEHAVIOR demos.
+    The set of demos is specified in a manifest file
+    """
+    logging.info("*" * 80 + "\nDescription:" + main.__doc__ + "*" * 80)
 
-    print(args)
+    defaults = selection == "random" and headless and short_exec
+    args_dict = parse_args(defaults=defaults)
 
     def get_imvotenet_callbacks(demo_name, out_dir):
         path = os.path.join(out_dir, demo_name + "_data.h5py")
@@ -292,10 +319,11 @@ def main(selection="user", headless=False, short_exec=False):
         )
 
     # TODO: Set resolution to match model.
+    logging.info("Generating dataset of observations (pointclouds and bounding boxes)")
     replay_demo_batch(
-        args.demo_root,
-        args.log_manifest,
-        args.out_dir,
+        args_dict["demo_root"],
+        args_dict["log_manifest"],
+        args_dict["out_dir"],
         get_imvotenet_callbacks,
         image_size=(480, 480),
         ignore_errors=True,
