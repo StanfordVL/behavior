bf049152df6e997e63b8f0547da32fa0df2188ba
Roberto Martin-Martin <robertom@cs.stanford.edu>
2022-02-05 22:45:47 -0800

    fixes and renaming

---
+++ behavior/examples/replay_demo_with_action_primitives_batch.py
@@ -0,0 +1,61 @@
+import argparse
+import glob
+import os
+import subprocess
+
+import igibson
+import tqdm
+
+
+def parse_args(defaults=False):
+    default_demo_directory = "fixme"
+    default_segmentation_directory = "fixme"
+    default_results_directory = "fixme"
+    args_dict = dict()
+    if not defaults:
+        parser = argparse.ArgumentParser(description="Script to batch-replay segmented demos using motion primitives.")
+        parser.add_argument("demo_directory", type=str, help="Path to directory containing demos")
+        parser.add_argument("segmentation_directory", type=str, help="Path to directory containing demo segmentations")
+        parser.add_argument("results_directory", type=str, help="Path to directory to store results in")
+        args = parser.parse_args()
+
+    args_dict["demo_directory"] = default_demo_directory if defaults else args.demo_directory
+    args_dict["segmentation_directory"] = default_segmentation_directory if defaults else args.segmentation_directory
+    args_dict["results_directory"] = default_results_directory if defaults else args.results_directory
+    return args_dict
+
+
+def main(selection="user", headless=False, short_exec=False):
+    """
+    Replays a batch of demos using action primitives
+    Creates threads for a batch of demos to be replayed in parallel
+    Uses the code in replay_demo_with_action_primitives.py
+    """
+    defaults = selection == "random" and headless and short_exec
+    args_dict = parse_args(defaults=defaults)
+
+    # Load the demo to get info
+    for demo_fullpath in tqdm.tqdm(list(glob.glob(os.path.join(args_dict["segmentation_directory"], "*.json")))):
+        demo = os.path.splitext(os.path.basename(demo_fullpath))[0]
+        if "replay" in demo:
+            continue
+
+        demo_path = os.path.join(args_dict["demo_directory"], demo + ".hdf5")
+        segmentation_path = os.path.join(args_dict["segmentation_directory"], demo + ".json")
+        output_path = os.path.join(args_dict["results_directory"], demo + ".json")
+        log_path = os.path.join(args_dict["results_directory"], demo + ".log")
+
+        if os.path.exists(output_path):
+            continue
+
+        # Batch me
+        script_path = os.path.join(igibson.example_path, "behavior", "replay_demo_with_action_primitives.py")
+        command = ["python", script_path, demo_path, segmentation_path, output_path]
+
+        with open(log_path, "w") as log_file:
+            tqdm.tqdm.write("Processing %s" % demo)
+            subprocess.run(command, stdout=log_file, stderr=subprocess.STDOUT)
+
+
+if __name__ == "__main__":
+    main()
